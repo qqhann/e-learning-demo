68cb6b87dfca0651421e9b5d8826fce3493ff68d
Qiushi Pan <qiu.gits@gmail.com>
2020-12-01 18:01:17 +0900

    Update: created the basic frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components'
 
-function App() {
+
+const Main = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Frame = styled.div`
+  width: 800px;
+  margin-bottom: 20px;
+  padding: 10px;
+  background-color: #dfdfdf;
+  border-radius: 8px;
+
+  display: grid;
+  grid-template-areas:
+    "que que"
+    "ans res"
+    "his pred";
+  gap: 8px;
+`
+const common = css`
+  background-color: white;
+  padding: 10px 16px;
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+`
+const Question = styled.div`
+  ${common}
+  grid-area: que;
+`
+const Answer = styled.div`
+  ${common}
+  grid-area: ans;
+`
+const Result = styled.div`
+  ${common}
+  grid-area: res;
+`
+const History = styled.div`
+  ${common}
+  grid-area: his;
+`
+const Prediction = styled.div`
+  ${common}
+  grid-area: pred;
+`
+
+const Navigation = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+const Button = styled.button`
+  border: none;
+  background-color: #4b75ff;
+  border-radius: 4px;
+  color: white;
+  margin: 4px;
+  padding: 8px;
+  cursor: pointer;
+`
+
+
+const App = () => {
+  const [question, setQuestion] = useState<string>('')
+  const [id, setId] = useState<number>(0)
+  useEffect(() => {
+    fetch('http://0.0.0.0:5000/question?id=' + id)
+      .then(res => res.json())
+      .then(res => { setQuestion(res.q) })
+  }, [id])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <Frame>
+        <Question>
+          <div dangerouslySetInnerHTML={{ __html: question }} />
+        </Question>
+        <Answer>1. Fine. 2. Bad</Answer>
+        <Result>True</Result>
+        <History>ooxxoxo</History>
+        <Prediction>0.74</Prediction>
+      </Frame>
+      <Navigation>
+        <Button onClick={() => { setId(id - 1) }}>Prev</Button>
+        <Button onClick={() => { setId(id + 1) }}>Next</Button>
+      </Navigation>
+    </Main>
   );
 }
 
